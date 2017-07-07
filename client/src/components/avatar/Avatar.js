bde81aae490131656e57ad7799a03bd26b1e8fe1
Daniel Baird <daniel@danielbaird.com>
2017-07-07 22:00:10 +1000

    client boilerplate

---
+++ client/src/components/avatar/Avatar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Avatar.css'
+
+class Avatar extends Component {
+  render() {
+    return (
+      <div className="Avatar">
+          <img src={this.props.url} alt="logo" />
+      </div>
+    )
+  }
+}
+
+export default Avatar
