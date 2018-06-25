683cd6d20823023d27d1748b0bb09478fd85c497
Daniel Baird <daniel@danielbaird.com>
2018-06-25 11:38:19 +1000

    adding footers

--- cra-client/src/components/layout/FinePrintFooter.js
+++ cra-client/src/components/layout/FinePrintFooter.js
@@ -3,15 +3,58 @@ import React, { Component } from 'react'
 
 import './FinePrintFooter.css'
 
+import TSIFlag from './TSIFlag.svg'
+import AAFlag from './AAFlag.svg'
+
+import AFULogo from './AFULogo.svg'
+import IRULogo from './IRULogo.svg'
+import UALogo from './UALogo.svg'
+import SotTLogo from './SotTLogo.svg'
+
 
 class FinePrintFooter extends Component {
 
   render() {
     return (
-      <p>fine print footer</p>
+      <footer class="FinePrintFooter clearfix">
+        <div class="traditional">
+          <div class="flag"><img src={AAFlag} /></div>
+          <p>
+            We acknowledge Australian Aboriginal People and
+            Torres Strait Islander People as the first
+            inhabitants of the nation, and acknowledge
+            Traditional Owners of the lands where our staff
+            and students live, learn and work.
+          </p>
+          <div class="flag"><img src={TSIFlag} /></div>
+        </div>
+
+        <div class="logos">
+          <a href=""><img src={AFULogo} /></a>
+          <a href=""><img src={IRULogo} /></a>
+          <a href=""><img src={UALogo} /></a>
+          <a href=""><img src={SotTLogo} /></a>
+        </div>
+
+        <ul class="copyright">
+          <li>Copyright &copy; 1995 to 2018 James Cook University.</li>
+          <li>All rights reserved.</li>
+          <li>ABN 46253211955</li>
+        </ul>
+
+        <ul class="legal">
+          <li><a href="">Feedback</a></li>
+          <li><a href="">Terms of use</a></li>
+          <li><a href="">Right to Information and Privacy</a></li>
+          <li><a href="">CRICOS Provider Code: 00117J</a></li>
+        </ul>
+
+      </footer>
 
     )
   }
+
+
 
 }
 
--- cra-client/src/components/layout/LinkFooter.js
+++ cra-client/src/components/layout/LinkFooter.js
@@ -8,23 +8,28 @@ class LinkFooter extends Component {
 
   render() {
 
-    const demoCategory = <div class="category">
-                          <h3>About JCU</h3>
+    const demoCategoryA = <div class="category"><h3>About JCU</h3>
                           <ul>
                             <li><a href="">Facts and Figures</a></li>
                             <li><a href="">Campuses and Locations</a></li>
-                          </ul>
-                        </div>
+                            <li><a href="">Organisational Structure</a></li>
+                          </ul></div>
+
+    const demoCategoryB = <div class="category"><h3>Contact Us</h3>
+                          <ul>
+                            <li><a href="">For Students</a></li>
+                            <li><a href="">For Staff</a></li>
+                          </ul></div>
 
 
     return (
       <footer class="LinkFooter clearfix">
         <div class="LinkFooter-inner">
-          {demoCategory}
-          {demoCategory}
-          {demoCategory}
-          {demoCategory}
-          {demoCategory}
+          {demoCategoryA}
+          {demoCategoryB}
+          {demoCategoryA}
+          {demoCategoryB}
+          {demoCategoryA}
         </div>
       </footer>
     )
